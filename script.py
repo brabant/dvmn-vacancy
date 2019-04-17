edc4b4c47698cd6bf548dfd8516d3dd4252024f5
Sergey <s.ilyin@pressfeed.ru>
2019-04-17 20:47:59 +0300

    revision 1

--- script.py
+++ script.py
@@ -5,6 +5,13 @@ import statistics
 import terminaltables
 
 
+SUPERJOB_CATALOGUE = 48
+
+
+class TownNotFound(BaseException):
+    pass
+
+
 def get_predict_salary(salary_from, salary_to):
     if salary_from in (None, 0) and salary_to in (None, 0):
         return None
@@ -24,9 +31,8 @@ def find_hh_area_recursive(areas, text):
             return area['id']
         if len(area['areas']) > 0:
             area_id = find_hh_area_recursive(area['areas'], text)
-            if area_id is not False:
+            if area_id is not None:
                 return area_id
-    return False
 
 
 def get_hh_area_id(text):
@@ -35,28 +41,26 @@ def get_hh_area_id(text):
     response.raise_for_status()
     area_id = find_hh_area_recursive(response.json(), text)
     if not area_id:
-        raise Exception('Город не найден')
+        raise TownNotFound('Город HeadHunter не найден')
     return area_id
 
 
 def get_hh_vacancies(text, area_id=None, period=None):
     vacancies = []
     url = "https://api.hh.ru/vacancies"
-    parameters = {'text': text,
-                  'per_page': 20,
-                  'page': 0,
-                  'area': area_id,
-                  'period': period}
+    params = {'text': text,
+              'per_page': 20,
+              'page': 0,
+              'area': area_id,
+              'period': period}
 
-    page = 0
     while True:
-        parameters['page'] = page
-        response = requests.get(url, params=parameters)
+        response = requests.get(url, params=params)
         response.raise_for_status()
         pages = response.json()['pages']
         vacancies += response.json()['items']
-        page += 1
-        if page >= pages:
+        params['page'] += 1
+        if params['page'] >= pages:
             break
 
     return vacancies
@@ -80,12 +84,7 @@ def get_salaries(vacancies, func):
 
 def predict_hh_programmers_vacancies(languages, area=None, period=None):
     vacancies = {}
-    if area is not None:
-        area_id = get_hh_area_id(area)
-        if area_id is False:
-            raise Exception(message='Населенный пункт не найден')
-    else:
-        area_id = None
+    area_id = get_hh_area_id(area) if area is not None else None
 
     for language in languages:
         vacancies_found = get_hh_vacancies(f'программист {language}', area_id, period)
@@ -103,7 +102,7 @@ def get_sj_area_id(keyword):
     response = requests.get(url, params=params)
     response.raise_for_status()
     if len(response.json()['objects']) < 1:
-        raise Exception('Город не найден')
+        raise TownNotFound('Город SuperJob не найден')
     return response.json()['objects'][0]['id']
 
 
@@ -115,15 +114,13 @@ def get_sj_vacancies(key, keyword, area_id=None, catalogues=None):
         params['town'] = area_id
 
     vacancies = []
-    page = 0
     while True:
-        params['page'] = page
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
         pages = response.json()['total'] // 100 + 1
         vacancies += response.json()['objects']
-        page += 1
-        if page >= pages:
+        params['page'] += 1
+        if params['page'] >= pages:
             break
     return vacancies
 
@@ -135,15 +132,12 @@ def predict_rub_salary_sj(vacancy):
     return get_predict_salary(vacancy['payment_from'], vacancy['payment_to'])
 
 
-def predict_sj_programmers_vacancies(key, languages, area=None, catalogues=None):
+def predict_sj_programmers_vacancies(key, languages, area=None, catalogue=None):
     vacancies = {}
-    if area is not None:
-        area_id = get_sj_area_id(area)
-    else:
-        area_id = None
+    area_id = get_sj_area_id(area) if area is not None else None
 
     for language in languages:
-        vacancies_found = get_sj_vacancies(key, f'программист {language}', area_id, catalogues)
+        vacancies_found = get_sj_vacancies(key, f'программист {language}', area_id, catalogue)
         salaries = list(filter(lambda x: x is not None, (predict_rub_salary_sj(v) for v in vacancies_found)))
         vacancies[language] = {"vacancies_found": len(vacancies_found),
                                "vacancies_processed": len(salaries),
@@ -157,10 +151,7 @@ def print_table(vacancies, title):
         ('Язык программирования', 'Найдено вакансий', 'Обработано вакансий', 'Средняя зарплата, руб.')
     ]
     for language in vacancies:
-        if vacancies[language] is not None:
-            data = (language, vacancies[language]['vacancies_found'], vacancies[language]['vacancies_processed'],
-                    vacancies[language]['average_salary'])
-            table_data.append(data)
+        table_data.append([language, ] + list(x[1] for x in vacancies[language].items()))
     table = terminaltables.AsciiTable(table_data, title)
     print(table.table)
 
@@ -174,9 +165,9 @@ def main():
 
         superjob_key = os.getenv('SUPERJOB_KEY', False)
         if superjob_key:
-            vacancies = predict_sj_programmers_vacancies(superjob_key, languages, 'Москва', 48)
+            vacancies = predict_sj_programmers_vacancies(superjob_key, languages, 'Москва', SUPERJOB_CATALOGUE)
             print_table(vacancies, 'SuperJob Moscow')
-    except Exception as e:
+    except TownNotFound as e:
         print(f'Error: {e}')
 
 
